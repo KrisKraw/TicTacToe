255a1176ee4e3a8f7523ca73ec95257d66caba96
kriskraw <chriscrawley80@gmail.com>
2022-04-06 14:50:13 -0400

    added SplashScreen and name prompts

--- src/com/javaforce/tictactoe/PieceType.java
+++ src/com/javaforce/tictactoe/PieceType.java
@@ -6,11 +6,11 @@ public enum PieceType {
     X(Game.getLargeXImage(),Game.getMediumXImage(),Game.getSmallXImage()),
     O(Game.getLargeOImage(),Game.getMediumOImage(),Game.getSmallOImage()),
     E(Game.getLargeOImage(),Game.getMediumOImage(),Game.getSmallOImage()),
-    X1(Game.getLargeXImage(),Game.getMediumXImage(),Game.getSmallXImage()),
+    X1(Game.getLargeXImage(),Game.getMediumXImage(),Game.getSmallXImage());
 
-    public final ImageIcon largeImage;
-    public final ImageIcon mediumImage;
-    public final ImageIcon smallImage;
+    private final ImageIcon largeImage;
+    private final ImageIcon mediumImage;
+    private final ImageIcon smallImage;
 
     private PieceType(ImageIcon largeImage, ImageIcon mediumImage, ImageIcon smallImage) {
         this.largeImage = largeImage;
